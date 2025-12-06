a1d46f57e28b41ceed499ece00e12ed12d8a5ce6
zaki <222313244@stis.ac.id>
2025-12-06 16:53:29 +0700

    ubah coment tracing

--- resources/views/pages/murid/games/tracing.blade.php
+++ resources/views/pages/murid/games/tracing.blade.php
@@ -67,7 +67,7 @@
         <!-- Grid Container -->
         <div id="letter-grid"
             class="w-full max-w-6xl px-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 md:gap-6">
-            <!-- Cards will be populated by JS -->
+            <!-- Di sini akan mucnul kartu  -->
         </div>
     </div>
 

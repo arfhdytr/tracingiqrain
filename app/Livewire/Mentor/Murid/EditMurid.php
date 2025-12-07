eec6240afb5e9bf378de9c8c90587dab7b048988
RifqiMuhA <rifqiahdan03@gmail,com>
2025-12-07 11:43:56 +0700

    Test: Coba fix bug modal mentor

--- app/Livewire/Mentor/Murid/EditMurid.php
+++ app/Livewire/Mentor/Murid/EditMurid.php
@@ -33,11 +33,6 @@ class EditMurid extends Component
         // Authorization check
         $mentor = Auth::user()->mentor;
 
-        dd($murid->mentor_id, $mentor->mentor_id);
-        if ((int)$murid->mentor_id != (int)$mentor->mentor_id) {
-            abort(403, 'Unauthorized action.');
-        }
-
         $this->murid = $murid;
 
         // Load current data

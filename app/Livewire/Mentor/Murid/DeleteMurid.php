7a2d8b48eb757473da3fce65da1c662af9852791
RifqiMuhA <rifqiahdan03@gmail,com>
2025-12-07 11:10:09 +0700

    Test: Coba fix bug modal mentor

--- app/Livewire/Mentor/Murid/DeleteMurid.php
+++ app/Livewire/Mentor/Murid/DeleteMurid.php
@@ -20,18 +20,7 @@ class DeleteMurid extends Component
 
     public function confirmDeletion()
     {
-        // Pastikan murid adalah murid binaan mentor yang login
-        $mentor = Auth::user()->mentor;
-
-        if ($this->murid->mentor_id !== $mentor->mentor_id) {
-            $this->dispatch('updated', [
-                'title' => 'Anda tidak memiliki akses untuk menghapus murid ini',
-                'icon' => 'error',
-                'iconColor' => 'red',
-            ]);
-            return;
-        }
-
+        // TEMPORARY DEBUG: Skip all validation
         $this->resetErrorBag();
         $this->confirmDelete = true;
     }
@@ -42,8 +31,8 @@ class DeleteMurid extends Component
             // Double check authorization
             $mentor = Auth::user()->mentor;
 
-            if ($this->murid->mentor_id !== $mentor->mentor_id) {
-                throw new \Exception('Unauthorized action');
+            if ((int)$this->murid->mentor_id !== (int)$mentor->mentor_id) {
+                throw new \Exception('Anda tidak memiliki akses untuk menghapus murid ini');
             }
 
             DB::beginTransaction();

86c3afef10c56f31d97e0d7cb8ad63b3b36d09dc
RifqiMuhA <rifqiahdan03@gmail,com>
2025-12-07 09:32:04 +0700

    Rev: Baliki posisi import alpine

--- resources/views/components/layouts/dashboard.blade.php
+++ resources/views/components/layouts/dashboard.blade.php
@@ -64,13 +64,12 @@
 
         </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
         @livewireScriptConfig
 
         @rappasoftTableScripts
 
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
 
         <script>
             // Definisikan Toast global
